a5aaa4b63ce6585536bf1e25733687f316d4e622
GamingCoder <coreoder@gmail.comm>
2014-03-12 20:37:46 +0100

    Wrote better version

---
+++ README.md
@@ -0,0 +1,8 @@
+# Zequals
+Zequalizing is a simple method to transform numbers like 97.253536 into simple numbers like 100.
+
+## Sample
+```js
+var zequals = require('zequals');
+console.log(zequals(97.254));	// Prints 100
+```
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 exports = module.exports = function (num){
-	var r = num;
-
-	while((r%10) !== 0) {
-		r = Math.round(r);
-		r = Math.round(r/10)*10;
+	// Always round one time first
+	var r = Math.round(num);
+	// 10 to the power of x
+	var x = 0;
+	
+	while(r.toString().length !== 1){
+		x++;
+		r = Math.round(r/10);
 	}
+
+	r = r * Math.pow(10, x);
+
+	// if r==0 => r=1
+	if (r === 0) r = 1;
 
 	return r;
 };
