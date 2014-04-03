7e91a92356d3ce6777c6ff87ab10d249d52bfba6
GamingCoder <coreoder@gmail.com>
2014-04-03 19:45:28 +0200

    Fix naming of tests

--- test/int.js
+++ test/int.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var zequal = require('../index.js');
 
 // 0-9
-test('0-9 should zequal 0-9', function(t) {
+test('0-9', function(t) {
 	t.plan(9);
 	for (var i = 0; i < 9; i++) {
 		t.equal(zequal(i), i);
@@ -10,7 +10,7 @@ test('0-9 should zequal 0-9', function(t) {
 });
 
 // 10-99
-test('10-99', function(t) {
+test('10-1000', function(t) {
 	t.plan(990);
 	for (var i = 10; i < 1000; i++) {
 		var first = parseInt(i.toString()[0]);
