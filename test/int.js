eb4b5b74cef664ac9fc14a6904eb1891effeca46
GamingCoder <coreoder@gmail.com>
2014-04-03 19:31:51 +0200

    Wrote tests for 10-10000

--- test/int.js
+++ test/int.js
@@ -8,3 +8,17 @@ test('0-9 should zequal 0-9', function(t) {
 		t.equal(zequal(i), i);
 	};
 });
+
+// 10-99
+test('10-99', function(t) {
+	t.plan(89);
+	for (var i = 10; i < 10000; i++) {
+		var first = parseInt(i.toString()[0]);
+		var second = parseInt(i.toString()[1]);
+
+		var ten = Math.pow(10, i.toString().length-1);
+
+		if(second < 5) t.equal(zequal(i), first*ten);
+		else t.equal(zequal(i), (first+1)*ten);
+	};
+});
